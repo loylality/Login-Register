8399592c8c61739028ed10b9d3fbf2dc72f316f1
Kawsar Mahmud Raz <kawsar01994269452@gmail.com>
2026-01-19 08:40:57 -0800

    Format connection.php for improved readability

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
-$dbName = "user_system"; // নিশ্চিত করো স্পেস নেই
+$dbName = "user_system"; 
 
 $conn = new mysqli($servername, $dbUsername, $dbPassword, $dbName);
 
@@ -10,4 +10,5 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
 
